cbce035e9cb63166daf6a12032d3187ec2fe41fb
Guilherme Souza Araújo <guilhermesouzae21@gmail.com>
2023-11-07 21:50:20 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Grid, Typography } from "@mui/material";
+
+const App = () => {
+  return (
+    <Grid container>
+      <Grid item xs={12} textAlign='center'>
+        <Typography variant="h1" color="white">
+          Olá
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default App;
