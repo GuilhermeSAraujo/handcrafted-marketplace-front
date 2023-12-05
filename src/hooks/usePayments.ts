4144ec36c0bff88ac5e644cca73ad2340c322ff5
Guilherme Souza Araújo <guilhermesouzae21@gmail.com>
2023-12-05 19:10:50 -0300

    chore: adjusting refetch react-query

--- src/hooks/usePayments.ts
+++ src/hooks/usePayments.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { useQuery } from 'react-query';
 import { PaymentData } from '../models/payments';
 const STALE_TIME = 1000 * 60 * 5;
+const REFETCH_INTERVAL = 1000 * 5;
 
 export const usePayments = () => {
 	return useQuery(
@@ -11,6 +12,7 @@ export const usePayments = () => {
 
 			return result.data;
 		}, {
-		staleTime: STALE_TIME
+		staleTime: STALE_TIME,
+		refetchInterval: REFETCH_INTERVAL,
 	});
 };
